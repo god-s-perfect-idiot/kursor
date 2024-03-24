eb8b4a73cb8c6880bca5e2fa1fcdb04aa95d44d8
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2024-03-24 11:16:44 +0000

    Update dependencies and styles

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
 
-import '../styles/globals.css'
+const ubuntu = Poppins({
+  weight: ["400"],
+  subsets: ["latin-ext"],
+});
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={ubuntu.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
